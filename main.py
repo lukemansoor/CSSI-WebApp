2132c64427a249726ba60871c9e591cd45082881
Luke Mansoor <lhm90064@gmail.com>
2019-07-26 09:15:39 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#main.py
+# the import section
+import webapp2
+
+# the handler section
+class MainPage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('Hello, World!') #the response
+
+class SecretPage(webapp2.RequestHandler):
+    def get(self): #get requests
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('<h1 style ="background-color=blue>This is my secret page</h1>')
+
+
+# the app configuration section
+app = webapp2.WSGIApplication([
+    ('/', MainPage), #this maps the root url to the Main Page Handler
+], debug=True)
