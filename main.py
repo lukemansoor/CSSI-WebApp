508e4a2589ff64a86dc93cc009363363872a93f5
Luke Mansoor <lmansoor@o251-08.csuchico.edu>
2019-07-30 15:39:56 -0700

    Recipe Finder class

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #main.py
-# the import section
+#the import section
 import webapp2
 import jinja2
+import io
 import os
+from google.cloud import vision
+from google.cloud.vision import types
 
 # this initializes the jinja2 environment
 the_jinja_env = jinja2.Environment(
@@ -10,6 +13,40 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+'''class run_quickstart(webapp2.RequestHandler):
+    def get(self):
+        # Instantiates a client
+        # [START vision_python_migration_client]
+        client = vision.ImageAnnotatorClient()
+        # [END vision_python_migration_client]
+
+        # The name of the image file to annotate
+        file_name = os.path.join(
+            os.path.dirname(__file__),
+            '/phonepicutres-TA.jpg')
+
+        # Loads the image into memory
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+
+        image = types.Image(content=content)
+
+        # Performs label detection on the image file
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+
+
+        print('Labels:')
+        for label in labels:
+            print(label.description)
+        # [END vision_quickstart]
+'''
+
+class RecipeFinder(webapp2.RequestHandler):
+    def get(self):
+        endpoint_url='https://api.spoonacular.com/recipes/search?query=' + searchquery + '?apiKey=97d098f7ed6849a5bf2377f5bc2cbfbf'
+        
+    
 # the handler section
 class MainPage(webapp2.RequestHandler):
     def get(self): #for a get request
